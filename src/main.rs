100f3f175d8bdab506610ef5343f27aace7ec8e6
Milind Sharma <milindsharma8@gmail.com>
2024-06-22 18:24:33 +0800

    Add some padding to layout

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{
-    prelude::Text,
     backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, List, ListItem, Paragraph},
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::{Block, Borders, List, ListItem, Padding, Paragraph, Widget},
     Terminal,
 };
 use std::io;
-
-// TODO: Implement the ui 
-// TODO: Get list of ports and display them
-// TODO: Make it possible to select a port and connect to it
-// TODO: Show port data in the right pane
-// TODO: Implement a way to send data to the port
-// TODO: Implement a way to save the data to a file
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -48,59 +40,65 @@ async fn run_app(
 ) -> Result<(), Box<dyn std::error::Error>> {
     loop {
         terminal.draw(|f| {
-            // Layout split betwen the left and right panes
+            // Layout split between the left and right panes
             let chunks = Layout::default()
                 .direction(Direction::Horizontal)
-                .margin(1)
-                .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+                .constraints([
+                    Constraint::Percentage(30),
+                    Constraint::Percentage(1),  // Add a padding between left and right panes
+                    Constraint::Percentage(68),
+                ].as_ref())
                 .split(f.size());
+
             // Left pane
             let ports_list = List::new(vec![
-                ListItem::new(Text::raw("COM1")),
-                ListItem::new(Text::raw("COM2")),
-                ListItem::new(Text::raw("COM3")),
-                ListItem::new(Text::raw("COM4")),
+                ListItem::new("COM1"),
+                ListItem::new("COM2"),
+                ListItem::new("COM3"),
+                ListItem::new("COM4"),
             ]).block(Block::default().title("Available Ports").borders(Borders::ALL));
             f.render_widget(ports_list, chunks[0]);  // Render these in left chunk/pane.
-            // Right pane
+
+            // Right pane layout: Filters, incoming data, input area
             let right_chunks = Layout::default()
                 .direction(Direction::Vertical)
-                .constraints(
-                    [
+                .constraints([
                     Constraint::Percentage(20),
                     Constraint::Percentage(60),
                     Constraint::Percentage(20),
-                    ]
-                    .as_ref()
-                )
-                .split(chunks[1]);
+                ].as_ref())
+                .split(chunks[2]);
+
             // Filters
             let filters = List::new(vec![
-                ListItem::new(Text::raw("Hex")),
-                ListItem::new(Text::raw("Binary")),
-                ListItem::new(Text::raw("Text")),
-                ListItem::new(Text::raw("Serial Plotter")),
+                ListItem::new("Hex"),
+                ListItem::new("Binary"),
+                ListItem::new("Text"),
+                ListItem::new("Serial Plotter"),
             ]).block(Block::default().title("Filters").borders(Borders::ALL));
-            f.render_widget(filters, right_chunks[0]);  // Render these in right chunk/pane, top.
 
             // Data
-            let incoming_data = Paragraph::new("Incoming data will be displayed here!").block(Block::default().title("Data").borders(Borders::ALL));
-            f.render_widget(incoming_data, right_chunks[1]);  // Render these in right chunk/pane, middle.
+            let incoming_data = Paragraph::new("Incoming data will be displayed here!")
+                .block(Block::default().title("Data").borders(Borders::ALL));
             
             // Send data
-            let input_area = Paragraph::new("Type data to send here!").block(Block::default().title("Send Data").borders(Borders::ALL));
-            f.render_widget(input_area, right_chunks[2]);  // Render these in right chunk/pane, bottom.
+            let input_area = Paragraph::new("Type data to send here!")
+                .block(Block::default().title("Send Data").borders(Borders::ALL));
+
+            // Render the right chunks
+            f.render_widget(filters, right_chunks[0]);
+            f.render_widget(incoming_data, right_chunks[1]);
+            f.render_widget(input_area, right_chunks[2]);
         })?;
 
         // Handle input
-
         if let Event::Key(event) = event::read()? {
             match event.code {
                 KeyCode::Char('q') => {
                     return Ok(());
                 }
                 // catch Ctrl+C
-                KeyCode::Char('c') if event.modifiers.contains(event::KeyModifiers::CONTROL) => {
+                KeyCode::Char('c') if event.modifiers.contains(KeyModifiers::CONTROL) => {
                     return Ok(());
                 }
                 _ => {}
@@ -108,6 +106,3 @@ async fn run_app(
         }
     }
 }
-
-
-
